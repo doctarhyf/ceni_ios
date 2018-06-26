867d69886287f2db0599f10bd8931f3919635215
Docta Rhyf <doctarhyf@doctas-imac.local>
2018-06-26 11:43:18 -0700

    Initial Commit

---
+++ CeniUITests/CeniUITests.swift
@@ -0,0 +1,36 @@
+//
+//  CeniUITests.swift
+//  CeniUITests
+//
+//  Created by Docta Rhyf on 6/26/18.
+//  Copyright © 2018 KOZI Engineering. All rights reserved.
+//
+
+import XCTest
+
+class CeniUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
